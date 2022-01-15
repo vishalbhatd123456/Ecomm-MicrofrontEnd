a0167433a882d3e1b8aeae78c0fb5f6384b34812
Vishal D Bhat <60818841+vishalbhatd123456@users.noreply.github.com>
2022-01-15 13:44:47 +0530

    Update index.js

--- products/index.js
+++ products/index.js
@@ -1,1 +1,14 @@
+import faker from 'faker';
 
+let products = '';
+
+for(let i = 0;i<3;i++){
+    const name = faker.commerce.productName();
+    //join the product
+    products+=`<div>${name}</div>`
+}
+
+console.log(products);  
+
+// this code when run on the browser will definitely shoot errors, as most of the browsers do NOT support the use of Faker library.
+//to overcome this, we can make use of the Webpack
